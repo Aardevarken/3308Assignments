5439ae6e7b492c6cc0bcba022025831b327eeca7
aardevarken <aardevarken@gmail.com>
2013-09-11 20:19:32 -0600

    modified dessert, shortened code.

--- Part1.rb
+++ Part1.rb
@@ -13,39 +13,47 @@ class Dessert
   	end
 
     def name
-    	return @name
+    	@name
     end
 
     def calories
-    	return @calories
+    	@calories
     end
 
     def healthy?
-        if @calories < 200; return true
+        if @calories < 200; true
         else
-        	return false
+        	false
         end
     end
 
     def delicious?
-        return true
+        true
     end
 end
 
-Cake = Dessert.new("cake", 200)
+# Cake = Dessert.new("cake", 200)
 
-puts Cake.name
-Cake.name=("pie")
-puts Cake.name
-puts Cake.calories
-puts Cake.healthy?
-puts Cake.delicious?
-Cake.calories=(100)
-puts Cake.healthy?
+# puts Cake.name
+# Cake.name=("pie")
+# puts Cake.name
+# puts Cake.calories
+# puts Cake.healthy?
+# puts Cake.delicious?
+# Cake.calories=(100)
+# puts Cake.healthy?
 
 class JellyBean < Dessert
     def initialize(name, calories, flavor)
         @flavor = flavor
+    end
+
+    def flavor=(flavor)
+    	@flavor = flavor
+    end
+
+    def flavor
+    	@flavor
     end
 
     def delicious?
