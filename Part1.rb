cd652343241022d4f96b1878805933e84ee16d53
aardevarken <aardevarken@gmail.com>
2013-09-11 19:57:42 -0600

    added JellyBean class framework

--- Part1.rb
+++ Part1.rb
@@ -1,9 +1,33 @@
 class Dessert
     def initialize(name, calories)
-        # Your code here
+        @name = name
+        @calories = calories
+    end
+
+    def name
+    	return @name
+    end
+
+    def calories
+    	return @calories
     end
 
     def healthy?
+        # Your code here
+    end
+
+    def delicious?
+        return true
+    end
+end
+
+Cake = Dessert.initialize("Cake", 200)
+
+puts Cake.name
+puts Cake.calories
+
+class JellyBean < Dessert
+    def initialize(name, calories, flavor)
         # Your code here
     end
 
